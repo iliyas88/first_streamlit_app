b9d6e8a51afe0419b4d08b5f53d9830bef8871a9
iliyas88 <130663290+iliyas88@users.noreply.github.com>
2023-05-30 15:58:49 +0530

    Update create_streamlit_app.py

--- create_streamlit_app.py
+++ create_streamlit_app.py
@@ -110,18 +110,19 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit load list contains:")
-streamlit.text(my_data_row)
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 
-# cmd=streamlit.text_input('custom query')
-# error=False
-# try:
-#     my_cur.execute(cmd)
-# except:
-#     error=True
-# if not error:
-#     streamlit.write(my_cur.fetchall())
-# else:
-#     streamlit.error('invalid query!!!')
+# my_data_row = my_cur.fetchone()
+# streamlit.text("The Fruit load list contains:")
+# streamlit.text(my_data_row)
+
+cmd=streamlit.text_input('custom query')
+error=False
+try:
+    my_cur.execute(cmd)
+except:
+    error=True
+if not error:
+    streamlit.write(my_cur.fetchall())
+else:
+    streamlit.error('invalid query!!!')
